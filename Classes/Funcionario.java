c9b971d1157d8d1edc94fca0c9488136b76f0d98
JoaoVitorHernandes <149633314+JoaoVitorHernandes@users.noreply.github.com>
2024-08-03 02:02:58 -0300

    Add files via upload
    Atualização da Classe Funcionario com salvamento de dados.

--- Classes/Funcionario.java
+++ Classes/Funcionario.java
@@ -1,8 +1,11 @@
+import java.io.*;
+
 public class Funcionario {
     private String nome;
     private int idade;
     private String telefone;
     private String email;
+
     public Funcionario(String nome, int idade, String telefone, String email) {
         this.nome = nome;
         this.idade = idade;
@@ -24,5 +27,50 @@ public class Funcionario {
 
     public String getEmail() {
         return email;
+    }
+
+    public static void listarVeiculos() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("Veiculos_Salvos.txt"));
+        String linha;
+        boolean hasContent = false;
+
+        System.out.println("Veículos cadastrados:");
+
+        while ((linha = reader.readLine()) != null) {
+            if (linha.trim().startsWith("Marca:")) {
+                if (hasContent) {
+                    System.out.println();
+                }
+                System.out.println("-Veículo-");
+                hasContent = true;
+            }
+            System.out.println(linha);
+        }
+        if (!hasContent) {
+            System.out.println("Nenhum veículo cadastrado.");
+        }
+        reader.close();
+    }
+
+    public static void listarClientes() throws IOException {
+        BufferedReader cl = new BufferedReader(new FileReader("Dados_Salvos.txt"));
+        String linhaCl;
+        boolean hasContent = false;
+
+        System.out.println("Clientes cadastrados:");
+        while ((linhaCl = cl.readLine()) != null) {
+            if (linhaCl.trim().startsWith("Nome:")) {
+                if (hasContent) {
+                    System.out.println();
+                }
+                System.out.println("-Cliente-");
+                hasContent = true;
+            }
+            System.out.println(linhaCl);
+        }
+        if (!hasContent) {
+            System.out.println("Nenhum cliente cadastrado.");
+        }
+        cl.close();
     }
 }
